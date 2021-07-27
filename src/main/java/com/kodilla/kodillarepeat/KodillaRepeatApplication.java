96dfbadc2c4ca9d6bffbc6feab20d33111f4cf12
lukkra1989 <luk.kramarczyk@gmail.com>
2021-07-27 12:58:07 +0200

    Adding test module

---
+++ src/main/java/com/kodilla/kodillarepeat/KodillaRepeatApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillarepeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaRepeatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaRepeatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/kodillarepeat/KodillaRepeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillarepeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaRepeatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
