837e92b5b41bed6de345dc78e5bef86c2c08962b
lukkra1989 <kodowanie2@o2.pl>
2021-07-27 13:17:47 +0200

    first commit

--- src/main/java/com/kodilla/kodillarepeat/testing/TestingMain.java
+++ src/main/java/com/kodilla/kodillarepeat/testing/TestingMain.java
@@ -2,6 +2,7 @@ package com.kodilla.kodillarepeat.testing;
 
 public class TestingMain {
     public static void main(String[] args) {
+
         System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
     }
 }
